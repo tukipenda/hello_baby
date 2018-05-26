34c73cfd83e9e0bf8425042c3af552683cd495f6
bearle2009 <bearle2009@gmail.com>
2018-05-26 15:02:54 -0400

    getting things working on codeanywhere

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn server:application --log-file -
+gunicorn server:application --log-file - --bind 0.0.0.0:5000
--- server.py
+++ server.py
@@ -6,3 +6,6 @@ from hello_baby import app
 application = SharedDataMiddleware(app, {
     '/static': os.path.join(os.path.dirname(__file__), 'static')
 })
+
+if __name__ == '__main__':    
+    app.run(debug=True, host='0.0.0.0')
