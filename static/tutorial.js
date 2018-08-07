2635f40c3ae2d3edb8d8af1fbc1ad3c6d517b469
bearle2009 <bearle2009@gmail.com>
2018-08-07 15:35:01 -0400

    getting tutorial working

--- static/tutorial.js
+++ static/tutorial.js
@@ -1,7 +1,23 @@
+tutorial_message=[
+    'First turn the warmer on.',
+    "These are the tabs you can use when you set up the warmer.  Remember that BIG RED BUTTON.  That's how you deliver the baby",
+    'You can adjust warmer settings here.  You can come back to the warmer tab to make more adjustments when the baby is delivered.',
+    "Now you can fetch supplies.",
+    "Time to deliver the baby.  Click the BIG RED BUTTON!",
+    "Okay, you just clicked that big red button, and now the baby has been delivered.  Here's what you do next!",
+    "These are the tabs you can use when the baby is delivered. You can also go back and review the history again if you need to with the history tab.",
+    "Here is the tab where you check exams.",
+    "This is the tab where you can pick actions to help the baby out.",
+    "Here's how you start PPV",
+    "You can adjust the settings for PPV before you get started",
+    "Yay, you finished the tutorial!  Click below to start the simulation!"
+]
+
 var tutorial = new Vue({
     el: '#TutorialApp',
     delimiters: ['[[',']]'],
     data: {
-      tutorial_img:"tutorial_1.png"
+      index:1,
+        tutorial_message:tutorial_message
     }
 });
