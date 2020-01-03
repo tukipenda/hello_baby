f71e090702481e66a6d313e936dec22868c465cc
bearle2009 <bearle2009@gmail.com>
2020-01-03 15:20:44 -0500

    thinking about next steps

---
+++ hello_baby_scenario/preemie_ppv_feedback.py
@@ -0,0 +1,17 @@
+#preemie_ppv feedback
+
+"""
+Prior to delivery:
+need to fetch pulse ox, 
+ETT tubes
+required settings for preemie
+
+For good NRP need:
+warm/dry/stimulate/suction infant
+start PPV in under 60 seconds
+do MRSOPA maneuvers
+reassess heart/lungs
+APGAR scores
+
+
+"""
